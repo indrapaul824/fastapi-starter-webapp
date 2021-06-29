73c0ac9c9845abacfeb72eef38ad54ce5ea1c707
IndraP24 <indrashis985@gmail.com>
2021-06-29 18:20:19 +0530

    Add the helper functions

--- app/library/helpers.py
+++ app/library/helpers.py
@@ -3,7 +3,7 @@ import markdown
 
 
 def openFile(file):
-    file_path = os.join("app/pages/", file)
+    file_path = os.path.join("app/pages/", file)
     with open(file_path, "r", encoding="utf-8") as input_file:
         text = input_file.read()
     
