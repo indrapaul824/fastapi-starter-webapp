f22974e74244a657587c902c426c80ddbc1d9c5e
IndraP24 <indrashis985@gmail.com>
2021-06-29 18:20:05 +0530

    Include routers

--- app/main.py
+++ app/main.py
@@ -2,10 +2,13 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from app.routers import unsplash
 
 from .library.helpers import *
 
 app = FastAPI()
+
+app.include_router(unsplash.router)
 
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
