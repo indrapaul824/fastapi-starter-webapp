308a2184f50f9b60b15229c990e569ef1f7461a5
IndraP24 <indrashis985@gmail.com>
2021-06-29 14:24:12 +0530

    Add a helper function to open and render markdown files

---
+++ app/library/helpers.py
@@ -0,0 +1,16 @@
+import os.path
+import markdown
+
+
+def openFile(file):
+    file_path = os.join("app/pages/", file)
+    with open(file_path, "r", encoding="utf-8") as input_file:
+        text = input_file.read()
+    
+
+    html = markdown.markdown(text)
+    data = {
+        "text": html
+    }
+
+    return data
