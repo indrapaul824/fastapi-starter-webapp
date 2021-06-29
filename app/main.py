30798b733bc608a65a4e676327b69926fc2358b6
IndraP24 <indrashis985@gmail.com>
2021-06-29 14:27:19 +0530

    Add helper function to main app

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
+from .library.helpers import *
+
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
@@ -10,18 +12,12 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
-    data = {
-        "page" : "Home Page!"
-    }
-
+    data = openFile("home.md")
     return templates.TemplateResponse("page.html", {"request": request, "data": data})
 
 
 
 @app.get("/page/{page_name}", response_class=HTMLResponse)
 async def page(request: Request, page_name: str):
-    data = {
-        "page" : page_name
-    }
-
+    data = openFile(page_name + ".md")
     return templates.TemplateResponse("page.html", {"request": request, "data": data})
