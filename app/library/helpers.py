474187b9829d2abe1b4726c8b0e0651557e502fa
IndraP24 <indrashis985@gmail.com>
2021-06-30 22:21:44 +0530

    Remove custom filter

--- app/library/helpers.py
+++ app/library/helpers.py
@@ -1,18 +1,5 @@
 import os.path
 import markdown
-import re
-from jinja2 import evalcontextfilter
-from markupsafe import Markup, escape
-
-_paragraph_re = re.compile(r'(?:\r\n|\r|\n){2,}')
-
-@evalcontextfilter
-def nl2br(eval_ctx, value):
-    result = u'\n\n'.join(u'<p>%s</p>' % p.replace('\n', Markup('<br>\n'))
-                          for p in _paragraph_re.split(escape(value)))
-    if eval_ctx.autoescape:
-        result = Markup(result)
-    return result
 
 
 def openFile(file):
