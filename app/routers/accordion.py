c51318c16ec8dc2f78b8fe60f46afa328b3c75a6
IndraP24 <indrashis985@gmail.com>
2021-07-01 00:27:51 +0530

    Add a post route

--- app/routers/accordion.py
+++ app/routers/accordion.py
@@ -1,5 +1,5 @@
-from types import resolve_bases
 from fastapi import FastAPI, Request, Form, APIRouter
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
@@ -10,3 +10,9 @@ templates = Jinja2Templates(directory="templates/")
 def get_accordion(request: Request):
     result = "Type a number"
     return templates.TemplateResponse('accordion.html', context={'request': request, 'result': result})
+
+
+@router.post("/accordion", response_class=HTMLResponse)
+def post_accordion(request: Request, tag: str = Form(...)):
+
+    return templates.TemplateResponse('accordion.html', context={'request': request, 'tag': tag})
